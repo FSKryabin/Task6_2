64dfcba1806484da26863d8d802881a42bd275c9
FSKryabin <95165120+FSKryabin@users.noreply.github.com>
2022-02-23 09:19:24 +0300

    Добавьте файлы проекта.

---
+++ Task6_2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Task6_2
+{
+    using System;
+ 
+ 
+ 
+    class Palindr
+     {
+        public static bool Palindrom (string s)
+        {
+            for (int i = 0, j = s.Length - 1; i < j; i++, j--)
+
+                if (s[i] != s[j])
+                        return false;
+                       return true;
+         }
+     
+    static void Main()
+        {
+        string s;
+            Console.WriteLine("Анализ палиндромов\n\nВведите строку:");
+          s = Console.ReadLine();
+           if (Palindrom(s))
+       Console.WriteLine("Эта строка - палиндром");
+       else Console.WriteLine("Эта строка - не палиндром");
+    Console.ReadLine();
+        }
+}
+}
